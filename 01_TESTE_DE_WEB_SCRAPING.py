240a2f462260cbb53e61a1f9dc4f0c470eeaeea5
Gabriel Costa <77176879+Vulkz@users.noreply.github.com>
2025-03-27 20:08:31 +0000

    import de biblioteca

--- 01_TESTE_DE_WEB_SCRAPING.py
+++ 01_TESTE_DE_WEB_SCRAPING.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import wget
 import zipfile
+import os
 
 url = "https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos"
 html = requests.get(url).content
