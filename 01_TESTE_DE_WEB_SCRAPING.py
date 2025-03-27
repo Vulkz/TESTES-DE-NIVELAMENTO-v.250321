ca36271d8235f74d3dd9a3e93f9061f3461242a1
Gabriel Costa <77176879+Vulkz@users.noreply.github.com>
2025-03-27 19:05:41 +0000

    concluido exercicio 01

---
+++ 01_TESTE_DE_WEB_SCRAPING.py
@@ -0,0 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
+import wget
+import zipfile
+
+url = "https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos"
+html = requests.get(url).content
+soup = BeautifulSoup(html, 'html.parser')
+
+elementos = soup.find_all('li')
+c = 1
+
+for elemento in elementos:
+    tag_a = elemento.find('a', class_='internal-link')
+
+    if tag_a:
+        wget.download(tag_a.get('href'),  f'Anexo{c}.pdf')
+        c+=1
+
+
+with zipfile.ZipFile('arquivo.zip', 'w') as zip:
+    zip.write('Anexo1.pdf')
+    zip.write('Anexo2.pdf')
