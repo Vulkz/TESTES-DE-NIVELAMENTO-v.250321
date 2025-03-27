53b437a0f74e9226294d2141c02ca33a611a9171
Gabriel Costa <77176879+Vulkz@users.noreply.github.com>
2025-03-27 20:07:34 +0000

    excluindo os arquivos em pdf

--- 01_TESTE_DE_WEB_SCRAPING.py
+++ 01_TESTE_DE_WEB_SCRAPING.py
@@ -17,7 +17,9 @@ for elemento in elementos:
         wget.download(tag_a.get('href'),  f'Anexo{c}.pdf')
         c+=1
 
-
 with zipfile.ZipFile('arquivo.zip', 'w') as zip:
     zip.write('Anexo1.pdf')
     zip.write('Anexo2.pdf')
+
+os.remove('Anexo1.pdf')
+os.remove('Anexo2.pdf')
